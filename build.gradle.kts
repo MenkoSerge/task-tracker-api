64a91534877dde3d617216ba7f5753ade0fd3e5d
Serge Menko <5er6emenk0@gmail.com>
2024-12-14 21:20:40 +0100

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "2.5.3"
 }
 
 group = "com.sergemenko"
@@ -8,7 +10,22 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+//Spring
+dependencies{
+    implementation("org.springframework.boot", "spring-boot-starter-web")
+    implementation("org.springframework.boot", "spring-boot-starter-data-jpa")
+}
+//Other
+dependencies{
+    implementation("org.postgresql","postgresql","42.2.19")
+}
+//Lombok
+dependencies{
+    compileOnly("org.projectlombok:lombok:1.18.20" )
+    testCompileOnly("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok:lombok:1.18.20")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.20")
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
